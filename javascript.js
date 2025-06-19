0ba6e00e119fb05c81ed2c7d751c4f519cf6279c
Althea Vestrit <137403706+AltheaVestrit@users.noreply.github.com>
2025-06-19 11:25:21 +0200

    Add UI to game

--- javascript.js
+++ javascript.js
@@ -1,3 +1,14 @@
+const buttons = document.querySelectorAll("button");
+const computerScoreSpan = document.querySelector("#computer_score");
+const humanScoreSpan = document.querySelector("#human_score");
+const updates = document.querySelector("#updates");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        playRound(button.id, getComputerChoice());
+    });
+});
+
 function getComputerChoice() {
     const num = Math.floor(Math.random() * 3) + 1;
     if (num == 1) {
@@ -9,34 +20,27 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    return prompt("Enter rock, paper or scissors as your choice: ");
-}
+let humanScore = 0;
+let computerScore = 0;
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-
-    function playRound(humanChoice, computerChoice) {
-        humanChoice = humanChoice.toLowerCase();
-        computerChoice = computerChoice;
-        if (humanChoice === computerChoice) {
-            console.log("It's a draw! You both chose " + humanChoice + ".");
-        } else if ((humanChoice == "rock" && computerChoice == "paper") || (humanChoice == "paper" && computerChoice == "scissors") || (humanChoice == "scissors" && computerChoice == "rock")) {
-            computerScore++;
-            console.log("You lose! " + computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1) + " beats " + humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1) + ".");
-        } else {
-            humanScore++;
-            console.log("You win! " + humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1) + " beats " + computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1) + ".");
-        }
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        updates.textContent = "It's a draw! You both chose " + humanChoice + ".";
+    } else if ((humanChoice == "rock" && computerChoice == "paper") || (humanChoice == "paper" && computerChoice == "scissors") || (humanChoice == "scissors" && computerChoice == "rock")) {
+        computerScore++;
+        computerScoreSpan.textContent = computerScore;
+        updates.textContent = "You lose! " + computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1) + " beats " + humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1) + ".";
+    } else {
+        humanScore++;
+        humanScoreSpan.textContent = humanScore;
+        updates.textContent = "You win! " + humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1) + " beats " + computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1) + ".";
     }
 
-    for (let i = 1; i < 6; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
-        console.log("Current scores: Computer - " + computerScore + "; Human - " + humanScore);
+    if (computerScore == 5 || humanScore == 5) {
+        updates.textContent += " GAME COMPLETE. Final score: computer - " + computerScore + "; you - " + humanScore + ".";
+        humanScore = 0;
+        computerScore = 0;
+        computerScoreSpan.textContent = computerScore;
+        humanScoreSpan.textContent = humanScore;
     }
-
-    console.log("Final scores: Computer - " + computerScore + "; Human - " + humanScore);
 }
-
-playGame();
